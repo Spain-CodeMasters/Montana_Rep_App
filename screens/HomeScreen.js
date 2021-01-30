4b584c30e38ceea2ae58167c100d39c58cbcdcea
Claire Spain <35749695+ClarissaSpain@users.noreply.github.com>
2021-01-30 13:02:59 -0700

    Small edits

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,12 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import {Text, View, Button, StyleSheet, TouchableOpacity, Dimensions, Platform, TextInput, StatusBar} from 'react-native';
+import {Text, View, Button, StyleSheet, TouchableOpacity, Dimensions, Platform, TextInput, StatusBar, ScrollView} from 'react-native';
 import * as Animatable from 'react-native-animatable';
 // import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
 // import { CheckBox } from 'react-native-elements'
-import Navigation from './components/navigation/navigation';
-import Settings from './components/settings/settings';
+import Navigation from '../components/navigation/navigation';
+import Settings from '../components/settings/settings';
 
 //Splash Banner
 //Our Mission
