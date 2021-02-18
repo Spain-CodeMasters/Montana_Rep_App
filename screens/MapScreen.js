6edc7a28d0ab111884cc9e77aa9c3f7e755ba695
Miya Fordah <42814756+mafordah@users.noreply.github.com>
2021-02-18 12:04:32 -0700

    added map navigation

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import {Text, View, Image, Button, StyleSheet, TouchableOpacity, Dimensions, Platform, TextInput, StatusBar} from 'react-native';
-import MapView, {PROVIDER_GOOGLE, Marker, Callout} from 'react-native-maps';
+import { Text, View, Image, Button, StyleSheet, TouchableOpacity, Dimensions, Platform, TextInput, StatusBar } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 import * as Animatable from 'react-native-animatable';
 // import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
@@ -24,57 +24,57 @@ import PlayingBanner from '../components/playingBanner';
 // const ITEM_WIDTH = width;
 // const ITEM_HEIGHT = height;
 
-const MapScreen = () => {
+export default ({ navigation }) => {
 
-    
-    return ( 
-      <View style={styles.container}>
-        <MapView
-        provider = {PROVIDER_GOOGLE}
-        style= {styles.map}
-        
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+
         region={{
           latitude: 46.87215,
           longitude: -113.994,
-           latitudeDelta: 0.00922,
-           longitudeDelta: 0.00921,
+          latitudeDelta: 0.00922,
+          longitudeDelta: 0.00921,
         }}
-        >
-          <Marker 
+      >
+        <Marker
           // coordinate={marker.coordinate}
           // title={marker.title}
           // description={marker.description}>
-            coordinate = {{
-              latitude: 46.86006,
-              longitude: -113.98523,
-            }}
-              image={require('../assets/map_marker.png')}
-              // title="Test Title"
-              // description= "This is the test description"
-          >
-            <Callout tooltip>
-              <View>
-                <View style={styles.bubble}>
-                  <Text style={styles.name}>Child's Play</Text>
-                  <Text style={styles.nameDescription}> A Short description</Text>
-                  {/* <Image
+          coordinate={{
+            latitude: 46.86006,
+            longitude: -113.98523,
+          }}
+          image={require('../assets/map_marker.png')}
+        // title="Test Title"
+        // description= "This is the test description"
+        >
+          <Callout tooltip>
+            <View>
+              <View style={styles.bubble}>
+                <Text style={styles.name}>Child's Play</Text>
+                <Text style={styles.nameDescription}> A Short description</Text>
+                {/* <Image
                   style={styles.image}
                   source={require('../assets/logo.png')}
                   /> */}
-                </View>
-                <View style={styles.arrowBorder}/>
-                <View style={styles.arrow}/>
               </View>
-            </Callout>
-          </Marker>
-        </MapView>
-        <Settings />
-       {/* <Navigation navigation={navigation}/> */}
-        </View>
-    );
+              <View style={styles.arrowBorder} />
+              <View style={styles.arrow} />
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <Settings />
+      <Navigation navigation={navigation} />
+    </View>
+  );
 };
 
-export default MapScreen;
+//export default MapScreen;
 
 const styles = StyleSheet.create({
   map: {
@@ -88,15 +88,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignSelf: 'flex-start',
     backgroundColor: '#fff',
-    borderRadius: 5, 
+    borderRadius: 5,
     borderColor: '#ccc',
     shadowColor: '#000',
     shadowRadius: 5,
     shadowOpacity: 0.3,
     shadowOffset: { x: 2, y: -2 },
-    paddingHorizontal:20, 
-    marginHorizontal:10,
-    height:35,
+    paddingHorizontal: 20,
+    marginHorizontal: 10,
+    height: 35,
     shadowColor: '#ccc',
     shadowOffset: { width: 0, height: 3 },
     shadowOpacity: 0.5,
@@ -104,14 +104,14 @@ const styles = StyleSheet.create({
     elevation: 10,
   },
   name: {
-    fontSize: 18, 
-    marginBottom: 5, 
+    fontSize: 18,
+    marginBottom: 5,
     fontFamily: 'FuturaPTBook',
     flexDirection: 'row'
-  }, 
+  },
   nameDescription: {
-    fontSize: 18, 
-    marginBottom: 5, 
+    fontSize: 18,
+    marginBottom: 5,
     fontFamily: 'FuturaPTBook',
     flexDirection: 'row'
   },
@@ -122,20 +122,20 @@ const styles = StyleSheet.create({
     borderWidth: 16,
     alignSelf: 'center',
     marginTop: -32
-    
+
   },
   arrowBorder: {
     backgroundColor: 'transparent',
     borderColor: 'transparent',
     borderTopColor: '#007a87',
-    borderWidth:16,
+    borderWidth: 16,
     alignSelf: 'center',
     marginTop: -0.5
   },
   image: {
     flex: 3,
     // alignSelf: 'center',
-    width:120,
+    width: 120,
     height: 80,
   }
 
