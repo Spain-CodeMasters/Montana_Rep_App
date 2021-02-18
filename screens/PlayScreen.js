8aa56d714fa2bd76fc6282e10cce4bcdef4e4731
Miya Fordah <42814756+mafordah@users.noreply.github.com>
2021-02-18 03:09:29 -0700

    working play/pause/lock

--- screens/PlayScreen.js
+++ screens/PlayScreen.js
@@ -13,9 +13,9 @@ const { width, height } = Dimensions.get('screen');
 const ITEM_WIDTH = width;
 const ITEM_HEIGHT = height * 0.9;
 
-const HEADER_MAX_HEIGHT = ITEM_HEIGHT;
-const HEADER_MIN_HEIGHT = 240;
-const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
+// const HEADER_MAX_HEIGHT = ITEM_HEIGHT;
+// const HEADER_MIN_HEIGHT = 240;
+// const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
 var author = 'Author';
 var title = 'Title';
@@ -23,37 +23,122 @@ var discription = "Play Discription";
 var transcript = "Play Transcript";
 
 {/* Video Testing */ }
-var source = 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4';
+//var source = 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4';
 
 {/* Audio Testing */ }
-// var source = 'https://actions.google.com/sounds/v1/crowds/voices_angry.ogg';
+var source = 'https://actions.google.com/sounds/v1/crowds/voices_angry.ogg';
 
 export default ({ navigation }) => {
-    const [scrollY, setScrollY] = useState(new Animated.Value(0));
-    const headerHeight = scrollY.interpolate({
-        inputRange: [0, HEADER_SCROLL_DISTANCE],
-        outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
-        extrapolate: 'clamp',
-    });
+    // const [scrollY, setScrollY] = useState(new Animated.Value(0));
+    // const headerHeight = scrollY.interpolate({
+    //     inputRange: [0, HEADER_SCROLL_DISTANCE],
+    //     outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+    //     extrapolate: 'clamp',
+    // });
 
     const video = useRef(null);
+    const [locked, setLocked] = useState(true);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [duration, setDuration] = useState(0);
+    const [fullScreen, setFullScreen] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [paused, setPaused] = useState(true);
+
+    const onProgress = (data) => {
+        // Video Player will progress continue even if it ends
+        if (!loading) {
+            setCurrentTime(data.currentTime);
+        }
+    };
+
+    const onLoad = (data) => {
+        setDuration(data.duration);
+        setLoading(false);
+    };
 
     return <View style={styles.container}>
 
 
         {/* MAIN CONTENT */}
         <ScrollView
-            style={{ position: 'relative' }}
-            scrollEventThrottle={16}
-            onScroll={Animated.event(
-                [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-                { useNativeDriver: false }
-            )}>
+        // style={{ position: 'relative' }}
+        // scrollEventThrottle={16}
+        // onScroll={Animated.event(
+        //     [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+        //     { useNativeDriver: false }
+        // )}
+        >
 
             <StatusBar backgroundColor='#fff' barStyle="dark-content" />
 
+            {/* AUDIO/VIDEO HEADER */}
+            <View style={styles.header} >
+                {/* <Animated.View style={[styles.header, { height: headerHeight }]} ></Animated.View> */}
+                <ImageBackground source={playImage} style={styles.image}>
+                    <View style={styles.overlay}>
+
+                        {/* video */}
+                        <Video source={{ uri: source }}
+                            ref={video}
+                            rate={1.0}
+                            volume={1.0}
+                            paused={paused}
+                            muted={false}
+                            resizeMode={"contain"}
+                            style={styles.video}
+                            onProgress={onProgress}
+                            onLoad={onLoad}
+                        />
+
+                        {/* title */}
+                        <Text style={styles.title}>{title}</Text>
+
+                        {/* preview */}
+                        {
+                            locked ? <TouchableOpacity
+                                onPress={() => setPaused(false)}
+                            >
+                                <View style={styles.button}>
+
+                                    <Text style={styles.buttonText}>Preview</Text>
+
+                                </View>
+                            </TouchableOpacity> : <View style={{ width: 177, height: 44, }}></View>
+                        }
+
+                        {/* controls */}
+                        {
+                            locked ? <TouchableOpacity onPress={() => { setLocked(false), alert('Unlocked') }}>
+                                <FontAwesome5
+                                    name="lock"
+                                    solid
+                                    color="#fff"
+                                    size={40}
+                                    style={{ padding: 10, }}
+                                />
+                            </TouchableOpacity> : null
+                        }
+
+                        {
+                            !locked ? <TouchableOpacity onPress={() => { paused ? setPaused(false) : setPaused(true) }}>
+                                <FontAwesome5
+                                    name={paused ? "play" : "pause"}
+                                    solid
+                                    color="#fff"
+                                    size={40}
+                                    style={{ padding: 10, }}
+                                />
+                            </TouchableOpacity> : null
+                        }
+
+
+
+                    </View>
+                </ImageBackground>
+            </View>
+
             {/* spacer */}
-            <View style={{ height: HEADER_MAX_HEIGHT }}></View>
+            {/* <View style={{ height: HEADER_MAX_HEIGHT }}></View> */}
 
             {/* discription */}
             <View style={styles.discription}>
@@ -72,52 +157,7 @@ export default ({ navigation }) => {
 
         </ScrollView>
 
-        {/* AUDIO/VIDEO HEADER */}
-        <Animated.View style={[styles.header, { height: headerHeight }]} >
-            <ImageBackground source={playImage} style={styles.image}>
-                <View style={styles.overlay}>
-                    {/* video */}
-                    <Video source={{ uri: source }}
-                        ref={video}
-                        rate={1.0}
-                        volume={1.0}
-                        paused={true}
-                        muted={false}
-                        resizeMode={"cover"}
-                        style={styles.video}
-                    />
 
-                    {/* title */}
-                    <Text style={styles.title}>{title}</Text>
-
-                    {/* preview */}
-                    <TouchableOpacity
-                        onPress={() => video.paused}
-                    >
-                        <View style={styles.button}>
-
-                            <Text style={styles.buttonText}>Preview</Text>
-
-                        </View>
-                    </TouchableOpacity>
-
-                    {/* controls */}
-                    <FontAwesome5
-                        name="lock"
-                        solid
-                        color="#fff"
-                        size={40}
-                        style={{ padding: 10, }}
-                    />
-                    {/* <FontAwesome5
-                        name="play"
-                        solid
-                        color="#fff"
-                        size={40}
-                    /> */}
-                </View>
-            </ImageBackground>
-        </Animated.View>
 
         <Settings />
         <Navigation navigation={navigation} />
@@ -129,12 +169,13 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     header: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
+        // position: 'absolute',
+        // top: 0,
+        // left: 0,
+        // right: 0,
         backgroundColor: '#fff',
         overflow: 'hidden',
+        height: ITEM_HEIGHT,
     },
     video: {
         position: 'absolute',
